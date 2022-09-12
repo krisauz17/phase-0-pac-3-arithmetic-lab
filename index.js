1c47516f8ee8c4e230f54f467d88351f9c283f1b
krishnendu basak <krisauz17@yahoo.com>
2022-09-13 09:58:14 +1000

    phase-0-pac-3-arithmetic-labis done

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let a, b;
+
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(a){
+    return a = a + 1;
+}
+function decrement(a){
+    return a = a - 1;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
